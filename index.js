cf9230d7d3f3a8274c94dd8dfac2b8ccbd0c3c66
Thomas Drevon <thomas@bengler.no>
2014-12-12 17:31:52 +0100

    Yup

--- index.js
+++ index.js
@@ -4,10 +4,15 @@ var app = express();
 var path = require('path');
 var routes = require('./routes');
 
+app.set('port', (process.env.PORT || 5000));
+app.use(express.static(__dirname + '/public'));
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
 app.use('/', routes);
 
+app.listen(app.get('port'), function() {
+  console.log("Node app is running at localhost:" + app.get('port'));
+});
 
 module.exports = app;
